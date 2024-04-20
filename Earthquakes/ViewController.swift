147a7db366f9a6e4c9f97a6c25ad305c45b55fb9
Natalie Hiehle <natalie.hiehle@gmail.com>
2024-04-20 19:37:34 -0400

    Initial Commit

---
+++ Earthquakes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Earthquakes
+//
+//  Created by Natalie Hiehle on 4/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
